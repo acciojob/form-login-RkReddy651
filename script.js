1bc5f39777da947277ca4430db590deb39b8d620
RkReddy651 <126152103+RkReddy651@users.noreply.github.com>
2023-12-09 16:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	var firstname = document.form1.fname.value;
+	var lastname = document.form1.lname.value;
+	// alert(`${firstname} ${lastname}`)
+	alert(firstname, lastname)
 }
